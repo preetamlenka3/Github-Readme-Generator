6b06cb5670f97f7de899be09263dec1dd700710e
P charan lenka <preetamlenka3@gmail.com>
2024-01-07 22:47:17 +0530

    Create templates.js

---
+++ templates.js
@@ -0,0 +1,62 @@
+// Template 1
+const template1 = `# GitHub Profile README
+
+Hello, I'm {{username}}. Welcome to my GitHub profile!
+
+## About Me
+
+Write a short introduction about yourself.
+
+## Skills
+
+- Skill 1
+- Skill 2
+- Skill 3
+
+## Connect with Me
+
+[![Twitter](https://img.shields.io/badge/Twitter-{{twitter}}-1DA1F2?style=flat-square&logo=twitter&logoColor=white)](https://twitter.com/{{twitter}})
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-{{linkedin}}-0077B5?style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/{{linkedin}})`;
+
+// Template 2
+const template2 = `# Welcome to My GitHub Profile
+
+Hey there! I'm {{username}}, a passionate developer.
+
+## About Me
+
+Write a short introduction about yourself.
+
+## Skills
+
+- Skill 1
+- Skill 2
+- Skill 3
+
+## Connect with Me
+
+[![Twitter](https://img.shields.io/badge/Twitter-{{twitter}}-1DA1F2?style=flat-square&logo=twitter&logoColor=white)](https://twitter.com/{{twitter}})
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-{{linkedin}}-0077B5?style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/{{linkedin}})`;
+
+// Template 3 (Add more templates as needed)
+const template3 = `# Custom GitHub Profile README
+
+Customize this template according to your preferences.
+
+## About Me
+
+Write a short introduction about yourself.
+
+## Skills
+
+- Skill 1
+- Skill 2
+- Skill 3
+
+## Connect with Me
+
+[![Twitter](https://img.shields.io/badge/Twitter-{{twitter}}-1DA1F2?style=flat-square&logo=twitter&logoColor=white)](https://twitter.com/{{twitter}})
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-{{linkedin}}-0077B5?style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/{{linkedin}})`;
+
+// Export the templates
+export { template1, template2, template3 };
