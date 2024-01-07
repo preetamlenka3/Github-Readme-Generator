337c628376186d821017af779281ca89d373272b
P charan lenka <preetamlenka3@gmail.com>
2024-01-07 23:13:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-// Templates
-const template1 = `# GitHub Profile README
+function generateReadme() {
+    const username = document.getElementById('username').value;
+    const template = document.getElementById('template').value;
+    const twitter = document.getElementById('twitter').value;
+    const linkedin = document.getElementById('linkedin').value;
 
-Hello, I'm {{username}}. Welcome to my GitHub profile!
+    // Retrieve the selected template content
+    let selectedTemplate;
+    if (template === 'template1') {
+        selectedTemplate = `# GitHub Profile README
+
+Hello, I'm ${username}. Welcome to my GitHub profile!
 
 ## About Me
 
@@ -15,32 +23,15 @@ Write a short introduction about yourself.
 
 ## Connect with Me
 
-[![Twitter](https://img.shields.io/badge/Twitter-{{twitter}}-1DA1F2?style=flat-square&logo=twitter&logoColor=white)](https://twitter.com/{{twitter}})
-[![LinkedIn](https://img.shields.io/badge/LinkedIn-{{linkedin}}-0077B5?style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/{{linkedin}})`;
-
-// Add more templates if needed
-
-function generateReadme() {
-    const username = document.getElementById('username').value;
-    const template = document.getElementById('template').value;
-
-    // Retrieve the selected template content
-    let selectedTemplate;
-    if (template === 'template1') {
-        selectedTemplate = template1;
-    } else {
-        // Add more conditions for additional templates if needed
-        // ...
-    }
-
-    // Retrieve social media links
-    const twitter = document.getElementById('twitter').value;
-    const linkedin = document.getElementById('linkedin').value;
+[![Twitter](https://img.shields.io/badge/Twitter-${twitter}-1DA1F2?style=flat-square&logo=twitter&logoColor=white)](https://twitter.com/${twitter})
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-${linkedin}-0077B5?style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/${linkedin})`;
+    } 
+    // Add more conditions for additional templates as needed
 
     // Replace placeholders with user input in the selected template
-    let readmeContent = selectedTemplate.replace(/{{username}}/g, username);
-    readmeContent = readmeContent.replace(/{{twitter}}/g, twitter);
-    readmeContent = readmeContent.replace(/{{linkedin}}/g, linkedin);
+    let readmeContent = selectedTemplate.replace(/\${username}/g, username);
+    readmeContent = readmeContent.replace(/\${twitter}/g, twitter);
+    readmeContent = readmeContent.replace(/\${linkedin}/g, linkedin);
 
     // Display the generated content in the textarea
     document.getElementById('output').value = readmeContent;
